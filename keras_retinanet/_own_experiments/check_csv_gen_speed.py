4cf8cc031f0f21141234ed11f929bcad718b4f63
bergermesa <berger@mesasight.com>
2022-06-09 19:15:16 +0200

    feat: implemented routine to check speed of data-generator from fizyr implementation

---
+++ keras_retinanet/_own_experiments/check_csv_gen_speed.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 8 13:39:00 2022
+
+@author: bergermesa
+"""
+# python packages
+import logging
+import numpy as np
+import os
+import sys
+import time
+from tqdm import tqdm
+
+# own packages
+import tools.debug as u_debug
+
+# Allow relative imports when being executed as script.
+if __name__ == "__main__" and __package__ is None:
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", ".."))
+    import keras_retinanet._own_experiments  # noqa: F401
+    __package__ = "keras_retinanet._own_experiments"
+
+# ensure the cython script gets built
+import pyximport
+pyximport.install(
+    setup_args={"include_dirs":np.get_include()},
+    reload_support=True
+)
+
+from ..preprocessing.csv_generator import CSVGenerator
+from ..utils.image import preprocess_image
+
+
+
+def main():
+    # User inputs
+    DATA_NAME = "dev-data_mini"
+    BATCH_SIZE = 1
+
+    # create bare minimum args object as needed for the fizyr implementation
+    class Args():
+        def __init__(self):
+            self.batch_size = BATCH_SIZE
+            self.config = None
+            self.image_min_side = 2560
+            self.image_max_side = 2560
+            self.no_resize = True
+            self.preprocess_image = preprocess_image
+            self.group_method = None
+            base_path = (
+                "/Users/marlinberger/Desktop/local/Coding/mesasight/"
+                + "keras-retinanet/keras_retinanet/_own_experiments/"
+                + f"data/{DATA_NAME}/"
+                )
+            self.annotations = base_path + "annotations.csv"
+            self.classes = base_path + "classes.csv"
+
+    # imitate fizyr 'create_generators' method as bare minimum
+    def create_generators(args):
+        # initialise the args needed for the CSVGenerator
+        common_args = {
+                'batch_size'       : args.batch_size,
+                'config'           : args.config,
+                'image_min_side'   : args.image_min_side,
+                'image_max_side'   : args.image_max_side,
+                'no_resize'        : args.no_resize,
+                'preprocess_image' : args.preprocess_image,
+                'group_method'     : args.group_method
+            }
+        # initialise the actual generator
+        train_generator = CSVGenerator(
+                    args.annotations,
+                    args.classes,
+                    shuffle_groups=False,
+                    **common_args
+                )
+        return(train_generator)
+
+    # get args and train-generator
+    args = Args()
+    train_generator = create_generators(args)
+
+    # evaluate the time the generator needs to process all images of the
+    # user-given database
+    logging.info(
+        "one step in the progress bar correlates with on batch processed"
+    )
+    s_t = time.time()
+    for item in tqdm(train_generator):
+        pass
+    f_t = time.time()
+    logging.info(f"Iterating over the database took {f_t-s_t:.1f} s")
+
+if __name__ == '__main__':
+    u_debug.initialise_logging("INFO")
+    main()
